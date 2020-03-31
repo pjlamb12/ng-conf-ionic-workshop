e3be6820459e6b75009e4be79c82342f41a17ee0
Preston Lamb <preston.j.lamb@gmail.com>
2020-03-31 13:51:48 -0600

    adding some stuff

--- ngConfWorkshop/src/app/app-routing.module.ts
+++ ngConfWorkshop/src/app/app-routing.module.ts
@@ -2,14 +2,24 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+	{ path: '', redirectTo: 'tabs', pathMatch: 'full' },
+	// { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomePageModule) },
+	// {
+	// 	path: 'detail/:id',
+	// 	loadChildren: () => import('./detail/detail.module').then(m => m.DetailPageModule),
+	// },
+	{
+		path: 'tabs',
+		loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule),
+	},
+	{
+		path: 'tab2',
+		loadChildren: () => import('./tab2/tab2.module').then(m => m.Tab2PageModule),
+	},
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- ngConfWorkshop/src/app/home/home.page.ts
+++ ngConfWorkshop/src/app/home/home.page.ts
@@ -1,12 +1,57 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AlertController, ModalController } from '@ionic/angular';
+import { ModalComponent } from '../modal/modal.component';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+	selector: 'app-home',
+	templateUrl: 'home.page.html',
+	styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+	items = [];
+	constructor(private _http: HttpClient, private alertCtrl: AlertController, private modalCtrl: ModalController) {}
 
-  constructor() {}
+	ionViewDidEnter() {
+		this.getNewItem().subscribe((response: any) => (this.items = response.results));
+	}
 
+	async showAlert(item) {
+		const alert = await this.alertCtrl.create({
+			message: 'show an alert',
+			subHeader: 'My subHeader',
+			header: 'my header',
+			buttons: [
+				{
+					role: 'Cancel',
+					text: 'Close',
+					handler: () => console.log('button clicked to close alert'),
+				},
+			],
+		});
+		alert.present();
+	}
+
+	async showModal(item) {
+		const modal = await this.modalCtrl.create({
+			component: ModalComponent,
+			componentProps: { item },
+		});
+
+		modal.present();
+	}
+
+	getNewItem() {
+		return this._http.get('https://randomuser.me/api');
+	}
+
+	doRefresh(evt) {
+		console.log(evt);
+		setTimeout(() => {
+			this.getNewItem().subscribe((response: any) => {
+				this.items.push(response.results[0]);
+				evt.detail.complete();
+			});
+		}, 2000);
+	}
 }
